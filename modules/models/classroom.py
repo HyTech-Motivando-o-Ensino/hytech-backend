183550edd79d21a0e828183835e9802c01518c7c
jeremias <jeremiasprogrammer@gmail.com>
2021-11-08 23:33:34 -0300

    added classroom class[

--- modules/models/classroom.py
+++ modules/models/classroom.py
@@ -3,3 +3,21 @@ class Classroom:
         self.__id = id
         self.__classroom_id = classroom_id
         self.__subject_id = subject_id
+
+    def get_id(self):
+        return self.__id
+
+    def get_classroom_id(self):
+        return self.__classroom_id
+
+    def get_subject_id(self):
+        return self.__subject_id
+
+    def set_id(self, id):
+        self.__id = id
+
+    def set_classroom_id(self, classroom_id):
+        self.__classroom_id = classroom_id
+
+    def set_subject_id(self, subject_id):
+        self.__subject_id = subject_id
---
+++ modules/models/tests/test_classroom.py
@@ -0,0 +1,19 @@
+from unittest import TestCase, main, mock
+try:
+    import sys
+    import os
+    sys.path.append(
+        os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__), '..'
+            )
+        )
+    )
+except:
+    raise
+from classroom import Classroom
+
+
+class TesteClassroom(TestCase):
+    def setUp(self):
+        self.classroom = Classroom()
