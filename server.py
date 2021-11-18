6da1b9fe41990de39bac06e0a6acba6c01c17c9d
jorgearaujo81 <jorge.araujo81@gmail.com>
2021-11-18 01:27:32 -0300

    update readme

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ app.add_middleware(
 
 @app.get("/")
 def index():
-    response = {"msg": "Hello World"}
+    response = {"msg": "Hello World!!"}
     
     return JSONResponse(response)
 
@@ -28,8 +28,128 @@ def status_db():
     
     return JSONResponse(response)
 
-@app.get("/create/")
-def index():
-    response = {"msg": "Hello World Jeremias"}
+@app.post("/set/name/")
+def set_name(request: Request):
+    try:
+        response = {"msg": "Como eu posso chamar você?"}
+    except Exception as e:
+        response = {"msg": "Não foi possível: {}".format(e)}
+    
+    return JSONResponse(response)
+
+@app.get("/get/name/")
+def get_name():
+    try:
+        response = {"msg": "Oi, Jorge"}
+    except Exception as e:
+        response = {"msg": "Não foi possível...: {}".format(e)}
+    
+    return JSONResponse(response)
+
+@app.post("/set/pronoun/")
+def set_pronoun(request: Request):
+    try:
+        response = {"msg": "Que pronome eu devo usar com você?"}
+    except Exception as e:
+        response = {"msg": "Não foi possível...: {}".format(e)}
+    
+    return JSONResponse(response)
+
+@app.get("/get/pronoun/")
+def get_pronoun():
+    try:
+        response = {"msg": "pronome feminino"}
+    except Exception as e:
+        response = {"msg": "Não foi possível...: {}".format(e)}
+    
+    return JSONResponse(response)
+
+@app.get("/get/zoom_class_link/")
+def get_zoom_class():
+    try:
+        response = {"msg": "https://cesar.zoom.us/j/94027650998"}
+    except Exception as e:
+        response = {"msg": "Não foi possível...: {}".format(e)}
+    
+    return JSONResponse(response)
+
+@app.post("/set/academic_period/")
+def set_academic_period(request: Request):
+    try:
+        response = {"msg": "Qual seu período?"}
+    except Exception as e:
+        response = {"msg": "Não foi possível...: {}".format(e)}
+    
+    return JSONResponse(response)
+
+@app.get("/get/academic_period/")
+def get_academic_period():
+    try:
+        response = {"msg": "2º"}
+    except Exception as e:
+        response = {"msg": "Não foi possível...: {}".format(e)}
+    
+    return JSONResponse(response)
+
+@app.post("/set/course/")
+def set_course(request: Request):
+    try:
+        response = {"msg": "Qual seu curso?"}
+    except Exception as e:
+        response = {"msg": "Não foi possível...: {}".format(e)}
+    
+    return JSONResponse(response)
+
+@app.get("/get/course/")
+def get_course():
+    try:
+        response = {"msg": "CC"}
+    except Exception as e:
+        response = {"msg": "Não foi possível...: {}".format(e)}
+    
+    return JSONResponse(response)
+
+@app.post("/set/class/")
+def set_class(request: Request):
+    try:
+        response = {"msg": "Qual a turma?"}
+    except Exception as e:
+        response = {"msg": "Não foi possível...: {}".format(e)}
+    
+    return JSONResponse(response)
+
+@app.get("/get/class/")
+def get_class():
+    try:
+        response = {"msg": "B"}
+    except Exception as e:
+        response = {"msg": "Não foi possível...: {}".format(e)}
+    
+    return JSONResponse(response)
+
+@app.get("/get/classroom_code/")
+def get_classroom_code():
+    try:
+        response = {"msg": "O código do classroom é..."}
+    except Exception as e:
+        response = {"msg": "Não foi possível...: {}".format(e)}
+    
+    return JSONResponse(response)
+
+@app.get("/get/notices/")
+def get_notices():
+    try:
+        response = {"msg": "Avisos:"}
+    except Exception as e:
+        response = {"msg": "Não foi possível...: {}".format(e)}
+    
+    return JSONResponse(response)
+
+@app.get("/get/notices/discipline/")
+def get_notices_discipline():
+    try:
+        response = {"msg": "Aviso da Disciplina..."}
+    except Exception as e:
+        response = {"msg": "Não foi possível...: {}".format(e)}
     
     return JSONResponse(response)
